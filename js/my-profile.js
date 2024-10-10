05223a0c1fedc90a6141a17d224b7c20df7e6e1b
etorriglia <etorrigliazimmer@gmail.com>
2024-10-10 11:21:42 -0300

    init

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,6 @@
+// const userInfoKeys= ["userFirstName", "userSecondName", "userLast", "userSecondLast", "userPhone"];
+
+
 // Funcion para mostrar el mail con el que ingreso el usuario en el campo E-mail
 showEmailValue()
 
@@ -7,7 +10,9 @@ function showEmailValue() {
   emailInput.value = emailValue;
 }
 
-
+function storeFieldValue(keyName, elementID){
+  return (storedField= localStorage.setItem(keyName, document.getElementById(elementID).value));
+}
 // Validacion del formulario al enviar
 (() => {
   'use strict'
@@ -19,6 +24,14 @@ function showEmailValue() {
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
+      } else{
+
+          let userFirstName= storeFieldValue('userFirstName', 'validationCustom01');
+          let userLast= storeFieldValue('userLast', 'validationCustom03');
+          let userSecondName= storeFieldValue('userSecondName', 'validationCustom02');
+          let userSecondLast= storeFieldValue('userSecondLast', 'validationCustom04');
+          let userPhone= storeFieldValue('userPhone', 'validationCustom06');
+
       }
 
       form.classList.add('was-validated')
