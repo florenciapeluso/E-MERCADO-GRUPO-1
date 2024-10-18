4694c4864e55215b5e10b0c0ac517d8bc296b27f
letipereira <letipereira3@gmail.com>
2024-10-18 17:20:15 -0300

    modified:   cart.html 	modified:   categories.html 	modified:   css/styles.css 	modified:   index.html 	modified:   js/cart.js 	modified:   js/categories.js modified:   js/index.js 	modified:   js/my-profile.js 	modified:   js/product-info.js 	modified:   js/products.js	modified:   js/sell.js 	modified:   my-profile.html 	modified:   product-info.html 	modified:   products.html 	modified:   sell.html

--- js/index.js
+++ js/index.js
@@ -13,3 +13,54 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
+
+// modo noche
+const user = getCookie('sessionUser'); 
+
+function enableDayMode() {
+  document.body.classList.add('day-mode');
+  document.body.classList.remove('night-mode');
+  localStorage.setItem(`${user}-theme`, 'day');
+  document.getElementById('theme-toggle').checked = false;
+
+  const elements = document.querySelectorAll(' .card-body, .navbar');
+  elements.forEach(el => {
+    el.classList.add('day-mode');
+    el.classList.remove('night-mode');
+  });
+
+}
+
+function enableNightMode() {
+  document.body.classList.add('night-mode');
+  document.body.classList.remove('day-mode');
+  localStorage.setItem(`${user}-theme`, 'night');
+  document.getElementById('theme-toggle').checked = true;
+
+  const elements = document.querySelectorAll('.card-body, .navbar');
+  elements.forEach(el => {
+    el.classList.add('night-mode');
+    el.classList.remove('day-mode');
+  });
+
+}
+
+function loadTheme() {
+  const savedTheme = localStorage.getItem(`${user}-theme`);
+  if (savedTheme === 'night') {
+    enableNightMode();
+  } else {
+    enableDayMode();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', loadTheme);
+
+// evento para el toggle de modo
+document.getElementById('theme-toggle').addEventListener('change', function () {
+  if (this.checked) {
+    enableNightMode();
+  } else {
+    enableDayMode();
+  }
+});
