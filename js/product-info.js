f836c000a1a2601603da4fef54978eab659c3778
letipereira <letipereira3@gmail.com>
2024-10-30 14:18:45 -0300

    modified:   css/styles.css 	modified:   js/index.js 	modified:   js/product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -286,7 +286,7 @@ function enableDayMode() {
   localStorage.setItem(`${user}-theme`, 'day');
   document.getElementById('theme-toggle').checked = false;
 
-  const elements = document.querySelectorAll('.card-body, .navbar'); //reconoce los elementos para agregarlos al css luego
+  const elements = document.querySelectorAll('.card-body, .navbar, .modal-body, .modal-header, .modal-footer'); //reconoce los elementos para agregarlos al css luego
   elements.forEach(el => {
     el.classList.add('day-mode');
     el.classList.remove('night-mode');
@@ -300,7 +300,7 @@ function enableNightMode() {
   localStorage.setItem(`${user}-theme`, 'night');
   document.getElementById('theme-toggle').checked = true;
 
-  const elements = document.querySelectorAll('.card-body, .navbar');
+  const elements = document.querySelectorAll('.card-body, .navbar, .modal-body, .modal-header, .modal-footer');
   elements.forEach(el => {
     el.classList.add('night-mode');
     el.classList.remove('day-mode');
